53d10afc5d1c38904c8aeae67dc15ef1422cd17e
Julien Bernard <jbernard@c2r1p3.42quebec.com>
2021-07-12 10:01:18 -0400

    Last libft push

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,24 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbernard <jbernard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jbernard <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/06 09:22:56 by jbernard          #+#    #+#             */
-/*   Updated: 2021/05/17 14:09:52 by jbernard         ###   ########.fr       */
+/*   Created: 2021/07/12 09:31:04 by jbernard          #+#    #+#             */
+/*   Updated: 2021/07/12 09:31:06 by jbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	while (n--)
+	int		i;
+	int		i2;
+
+	i = 0;
+	i2 = 0;
+	if (s != NULL && f != NULL)
 	{
-		*(char *)dest++ = *(char *)src;
-		if (*(unsigned char *)src++ == (unsigned char)c)
-			return (dest);
+		i = ft_strlen(s);
+		while (i2 < i)
+		{
+			(*f)(i2, s);
+			s++;
+			i2++;
+		}
 	}
-	return (0);
 }
--- libft.h
+++ libft.h
@@ -24,10 +24,9 @@ typedef struct		s_list
 
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *str, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
+void 	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_calloc(size_t nb, size_t size);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr_fd(int n, int fd);
@@ -38,6 +37,8 @@ void	ft_lstadd_back(t_list **alst, t_list *new);
 void 	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+
 
 int		ft_memcmp(const void *str1, const void *str2, size_t n);
 int		ft_isalpha(int c);
