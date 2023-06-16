f3971c2c7d1e5b6313f89d4ff80b2c5ee3980634
Julien Bernard <jbernard@c2r4p6.42quebec.com>
2023-06-16 13:38:51 -0400

    OK

--- src/ft_sfree.c
+++ src/ft_sfree.c
@@ -6,7 +6,7 @@
 /*   By: jbernard <jbernard@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 12:04:28 by jbernard          #+#    #+#             */
-/*   Updated: 2023/06/14 13:33:48 by jbernard         ###   ########.fr       */
+/*   Updated: 2023/06/14 15:56:58 by jbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	ft_sfree(void *tofree)
 {
 	if (tofree)
 	{
-		printf("\nFREE(%s)\n", tofree);
 		free(tofree);
 		tofree = NULL;
 	}
